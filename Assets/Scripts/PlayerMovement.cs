f065592e07df0adce77472dfbecc40f4580702f0
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2024-11-03 14:00:55 +0700

    make idle and walk animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Rigidbody2D playerRigidBody;
+    [SerializeField] private Animator playerAnim;
   
     // Update is called once per frame
     void FixedUpdate()
@@ -17,7 +18,18 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        manageDirection(horizontal, vertical);
+        playerAnim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        playerAnim.SetFloat("vertical", Mathf.Abs(vertical));
 
         playerRigidBody.velocity = new Vector2(horizontal, vertical) * speed; 
+    }
+
+    void manageDirection(float horizontal, float vertical)
+    {
+        if((horizontal > 0 && transform.localScale.x < 0) || (horizontal < 0 && transform.localScale.x > 0))
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
     }
 }
