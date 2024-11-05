481beae47de474437e4d9213c3be276fd066bf80
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2024-11-05 10:31:39 +0700

    implement camera follow using scripts

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraFollow();
+    }
+
+    void cameraFollow()
+    {
+        float playerX = player.transform.position.x;
+        float playerY = player.transform.position.y;
+        transform.position = new Vector3(playerX, playerY,transform.position.z);
+    }
+}
