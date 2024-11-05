d7ea9b2dcbbfcb77cad86db10800cbaf75dfe241
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2024-11-05 13:53:28 +0700

    Add health change animation

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int currentHealth;
     [SerializeField] private int maxHealth;
+    [SerializeField] private TMP_Text healthText;
+    [SerializeField] private Animator healthTextAnim;
 
+    private void Start()
+    {
+        healthText.text = "HP : " + currentHealth + " / " + maxHealth;
+    }
+
+    
     public void changeHealth(int health)
     {
         currentHealth += health;
+        healthTextAnim.Play("Text_Update");
+        healthText.text = "HP : " + currentHealth + " / " + maxHealth;
 
-        if(currentHealth <= 0)
+        if (currentHealth <= 0)
         {
             Destroy(this.gameObject);
         }
