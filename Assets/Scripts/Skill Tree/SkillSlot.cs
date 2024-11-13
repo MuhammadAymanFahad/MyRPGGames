d04dd792c794e8c1038edbd94b8476481ded9239
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2024-11-14 04:24:46 +0700

    implementing skill level up system UI

--- Assets/Scripts/Skill Tree/SkillSlot.cs
+++ Assets/Scripts/Skill Tree/SkillSlot.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SkillSlot : MonoBehaviour
 {
     public SkillSO skillSO;
+
+    public int currentLevel;
+    public bool isUnlocked;
+
     public Image skillIcon;
+    public TMP_Text skillLevelText;
 
     private void OnValidate()
     {
-        if(skillSO != null)
+        if(skillSO != null && skillLevelText != null)
         {
-            skillIcon.sprite = skillSO.skillIcon;
+            updateUI();
+        }
+    }
+
+    private void updateUI()
+    {
+        skillIcon.sprite = skillSO.skillIcon;
+        if (isUnlocked)
+        {
+            skillLevelText.text = currentLevel.ToString() + "/" + skillSO.maxLevel.ToString();
+            skillIcon.color = Color.white;
+        }
+        else
+        {
+            skillLevelText.text = "Locked";
+            skillIcon.color = Color.grey;
         }
     }
 }
