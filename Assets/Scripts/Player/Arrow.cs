c06170189b3b725e4c285c2a104e78ed73865c9f
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2024-11-16 22:37:20 +0700

    start arrow setup

---
+++ Assets/Scripts/Player/Arrow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D arrowRigidBody;
+    [SerializeField] private Vector2 direction;
+    void Start()
+    {
+        
+    }
+
+    
+}
