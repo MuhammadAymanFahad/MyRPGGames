a5604c0954332721da13c1c7a6bd05abd57e9e12
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2024-11-16 22:43:39 +0700

    check arrow movement test

--- Assets/Scripts/Player/Arrow.cs
+++ Assets/Scripts/Player/Arrow.cs
@@ -6,9 +6,12 @@ public class Arrow : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D arrowRigidBody;
     [SerializeField] private Vector2 direction;
+    [SerializeField] private float lifeSpan;
+    [SerializeField] private float speed;
     void Start()
     {
-        
+        arrowRigidBody.velocity = direction * speed;
+        Destroy(this.gameObject, lifeSpan);
     }
 
     
