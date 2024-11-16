073380f783b94d37f1667cb07da238cbb358c2c7
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2024-11-16 22:54:58 +0700

    arrow instance test

---
+++ Assets/Scripts/Player/PlayerBow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBow : MonoBehaviour
+{
+    [SerializeField] private Transform launchPoint;
+    [SerializeField] private GameObject arrowPrefabs;
+
+    private void Update()
+    {
+        shoot();
+    }
+
+    public void shoot()
+    {
+        Instantiate(arrowPrefabs, launchPoint.position, Quaternion.identity);
+    }
+}
