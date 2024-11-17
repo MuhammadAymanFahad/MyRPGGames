ac1e4803a8c94a4bbc8a590d5d93e636b1462b83
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2024-11-17 22:48:09 +0700

    arrow damage implementation

--- Assets/Scripts/Player/Arrow.cs
+++ Assets/Scripts/Player/Arrow.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D arrowRigidBody;
+    [SerializeField] private LayerMask enemyLayer;
     [SerializeField] private float lifeSpan;
     [SerializeField] private float speed;
+    [SerializeField] private int damage;
     public Vector2 direction = Vector2.right;
 
     void Start()
@@ -20,5 +22,13 @@ public class Arrow : MonoBehaviour
     {
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if((enemyLayer.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            collision.gameObject.GetComponent<EnemyHealth>().changeHealth(-damage);
+        }
     }
 }
